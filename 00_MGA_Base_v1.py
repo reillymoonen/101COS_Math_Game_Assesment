712b66028e8f71144b86e34a6032e9de6ba46392
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-08-02 13:46:42 +1200

    Added question solver

--- 00_MGA_Base_v1.py
+++ 00_MGA_Base_v1.py
@@ -74,6 +74,15 @@ def distribute_level(level):
         return level_3_generator()
 
 
+def question_solver(num1, num2):
+    correct_answer = num1 + num2
+    user_answer = int(input("Your answer: "))
+
+    if user_answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"Sorry the correct answer is {correct_answer}.")
+
 # Main Routine
 
 show_instructions = yes_no("Have you played the "
@@ -85,9 +94,12 @@ print()
 
 if choose_level == 1:
     l1num1, l1num2 = distribute_level(choose_level)
+    question_solver(l1num1, l1num2)
 elif choose_level == 2:
     l2num1, l2num2 = distribute_level(choose_level)
+    question_solver(l2num1, l2num2)
 elif choose_level == 3:
     l3num1, l3num2 = distribute_level(choose_level)
+    question_solver(l3num1, l3num2)
 else:
     print("Invalid level chosen.")
