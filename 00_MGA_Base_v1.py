9b82473d49c888e4936ff50c45387438dff26536
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-08-04 09:07:01 +1200

    Added game summarizer

--- 00_MGA_Base_v1.py
+++ 00_MGA_Base_v1.py
@@ -1,10 +1,10 @@
 import random
 
 
-def yes_no(questions):
+def yes_no(question):
     valid = False
     while not valid:
-        response = input(questions).lower()
+        response = input(question).lower()
         error = "Please answer yes / no"
 
         if response == "yes" or response == "y":
@@ -22,21 +22,16 @@ def yes_no(questions):
 
 
 def num_check(question, low, high):
-    error = "Please enter a whole number between 1 and 3\n"
+    error = f"Please enter a whole number between {low} and {high}\n"
 
     valid = False
     while not valid:
         try:
-            # ask the question
             response = int(input(question))
-            # if the amount is too low / too high give
-            if low < response <= high:
+            if low <= response <= high:
                 return response
-
-            # output an error
             else:
                 print(error)
-
         except ValueError:
             print(error)
 
@@ -44,7 +39,6 @@ def num_check(question, low, high):
 def level_1_generator():
     number_a = random.randint(1, 9)
     number_b = random.randint(1, 9)
-
     print(f"What is {number_a} + {number_b}?")
     return number_a, number_b
 
@@ -52,7 +46,6 @@ def level_1_generator():
 def level_2_generator():
     number_a = random.randint(10, 99)
     number_b = random.randint(10, 99)
-
     print(f"What is {number_a} + {number_b}?")
     return number_a, number_b
 
@@ -60,7 +53,6 @@ def level_2_generator():
 def level_3_generator():
     number_a = random.randint(100, 999)
     number_b = random.randint(100, 999)
-
     print(f"What is {number_a} + {number_b}?")
     return number_a, number_b
 
@@ -76,22 +68,29 @@ def distribute_level(level):
 
 def question_solver(num1, num2):
     correct_answer = num1 + num2
-
     while True:
         user_answer = input("Your answer: ")
-
         if user_answer.isdigit():
             if int(user_answer) == correct_answer:
                 print("Correct!")
+                return True
             else:
                 print(f"Sorry, the correct answer is {correct_answer}.")
-            break
+                return False
         else:
             print("Please enter a valid numeric answer.")
 
 
-# Main Routine
+def game_summary(correct_answers, total_questions):
+    print("\nGame Summary:")
+    print("---------------")
+    print(f"Total questions: {total_questions}")
+    print(f"Correct answers: {correct_answers}")
+    accuracy = (correct_answers / total_questions) * 100
+    print(f"Accuracy: {accuracy:.2f}%")
 
+
+# Main Routine
 show_instructions = yes_no("Have you played the "
                            "game before?\n")
 choose_level = num_check("What Level would you like to play"
@@ -99,21 +98,29 @@ choose_level = num_check("What Level would you like to play"
 print("You have selected level {}".format(choose_level))
 print()
 
+correct_answers = 0
+total_questions = 10
+
 if choose_level == 1:
-    for _ in range(10):
+    for _ in range(total_questions):
         l1num1, l1num2 = distribute_level(choose_level)
-        question_solver(l1num1, l1num2)
+        if question_solver(l1num1, l1num2):
+            correct_answers += 1
     else:
         print("Invalid level chosen.")
 elif choose_level == 2:
-    for _ in range(10):
+    for _ in range(total_questions):
         l2num1, l2num2 = distribute_level(choose_level)
-        question_solver(l2num1, l2num2)
+        if question_solver(l2num1, l2num2):
+            correct_answers += 1
     else:
         print("Invalid level chosen.")
 elif choose_level == 3:
-    for _ in range(10):
+    for _ in range(total_questions):
         l3num1, l3num2 = distribute_level(choose_level)
-        question_solver(l3num1, l3num2)
+        if question_solver(l3num1, l3num2):
+            correct_answers += 1
     else:
         print("Invalid level chosen.")
+
+game_summary(correct_answers, total_questions)
