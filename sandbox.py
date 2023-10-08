08076e1761bf06cde283c6329166da6697d2a1a3
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-10-09 10:58:25 +1300

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,20 @@
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+def generate_primes():
+    primes = []
+    yield 2  # 2 is the first prime number
+    candidate = 3
 
+    while True:
+        is_prime = all(candidate % prime != 0 for prime in primes)
+        if is_prime:
+            primes.append(candidate)
+            yield candidate
 
-for i in range(10):
-    print(fibonacci(i))
+        candidate += 2  # Skip even numbers (except for 2)
+
+# Example: Generate the first 10 prime numbers
+limit = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+prime_generator = generate_primes()
+
+for _ in range(limit):
+    print(next(prime_generator))
+
